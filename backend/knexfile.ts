7c332605cb62987fcda37cd241e8a5330be78283
root <root@sudip.sudip>
2024-07-22 20:58:05 +0545

    creating migration files

---
+++ backend/knexfile.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
