805cb2287250d504bb95740f4105ffe4cb7f049b
root <sudipkhati2043@gmail.com>
2024-07-25 23:35:55 +0545

    changes on migrations

--- backend/src/migrations/20240722150842_user.ts
+++ backend/src/migrations/20240722150842_user.ts
@@ -3,10 +3,10 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("users", (table) => {
     table.increments("id").primary().unique();
-    table.string("first_name");
-    table.string("last_name");
+    table.string("name_of_company");
     table.string("phone");
-    table.string("email");
+    table.string("company_email");
+    table.string("company_address");
     table.string("image");
     table.string("password");
     table.timestamps(true, true);
