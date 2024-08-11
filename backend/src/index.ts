6191dc5a97391f4b1115a826f750a7086e2e196d
root <sudipkhati2043@gmail.com>
2024-08-11 07:15:02 +0545

    updating employee and doing cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import companyRoute from "./routes/company/index";
 import jobSeeker from "./routes/job_seeker/index";
 import adminRouter from "./routes/admin/index";
 import path from "path";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -13,11 +14,22 @@ app.use(express.json());
 app.use("/css", express.static(path.join(__dirname, "../../css")));
 app.use("/js", express.static(path.join(__dirname, "../../js")));
 app.use("/images", express.static(path.join(__dirname, "../../images")));
+app.use("/image", express.static("public/"));
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
+
 app.get("/", (req: any, res: any) => {
   res.sendFile(path.join(__dirname, "../../home.html"));
 });
-app.use("/image", express.static("public/"));
-
 app.use("/category", categoryRoute);
 app.use("/company", companyRoute);
 app.use("/job_seeker", jobSeeker);
